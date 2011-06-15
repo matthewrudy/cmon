c6e1f73979b6774079c6d13dc307cc179b43b57f
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-06-15 20:17:38 +0800

    a working example cmon /some/path/to/my.cmon

--- lib/cmon.rb
+++ lib/cmon.rb
@@ -1,4 +1,41 @@
 require 'cmon/version'
 
 module CMon
+  class Command
+    def initialize(cmd)
+      @command = cmd
+      @output  = `#{cmd}`
+    end
+    attr_reader :command, :output
+
+    def should_not_include(text)
+      if self.output.include?(text)
+        puts "CMon assumptions failed"
+        puts "Should Not Include #{text.inspect}"
+        puts
+        puts "Output:"
+        puts self.output
+        exit 1
+      end
+    end
+  end
+  
+  class Runner
+    
+    def self.run(path)
+      runner = self.new
+      runner.run(path)
+    end
+    
+    def run(file)
+      steps = File.read(file)
+      self.instance_eval(steps)
+      exit 0
+    end
+    
+    def output_from(cmd)
+      CMon::Command.new(cmd)
+    end
+  end
+  
 end
